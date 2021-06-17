01d11eab03719f44acc738efad4d081ecbd77887
promd416@naver.com <promd416@naver.com>
2021-06-17 10:04:31 +0900

    js추가파일

--- js/event.js
+++ js/event.js
@@ -1,10 +1,17 @@
 //상세 설명 보기 클릭시 텍스트 보이기
 function showText(){
-    var text = document.getElementById("desc");
-    text.style.display = "block";
+    //var text = document.getElementById("desc");
+    //text.style.display = "block";
+    document.getElementById("desc").style.display="block";
+
+    //var button = document.getElementById("open");
+    //button.style.display = "none";
+    document.getElementById("open").style.display="none";
 }
 
 function hideText(){
     var text = document.getElementById("desc");
     text.style.display = "none";
+    var button = document.getElementById("open");
+    button.style.display = "block";
 }
